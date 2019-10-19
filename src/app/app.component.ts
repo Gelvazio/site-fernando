bec286811060187bf8d43422ea5380e215210986
maschinedreams <gabrielle.clima23@gmail.com>
2019-10-18 21:14:53 -0300

    refazer back-end com services e observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faBitcoin, faEthereum } from '@fortawesome/free-brands-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { crypto } from "./backend/crypto";
-import { constructor } from 'typescript';
-
-
-interface crypto {
-  name: { initials: string; }
-  price: { value: Float32Array; }
-}
 
 
 @Component({
@@ -17,12 +9,9 @@ interface crypto {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Fernando Limas P2P';
-  crypto: Crypto | null = null;
 }
-constructor(Crypto, Crypto); {};
 
 library.add(faBitcoin, faEthereum);
-ngOnInit ()  {
 
-}
